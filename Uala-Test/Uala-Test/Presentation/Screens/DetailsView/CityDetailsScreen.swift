e44d76031e73de07c4513ea054263873e667f272
Gastón Montes <gastonmontes@hotmail.com>
2025-01-09 18:29:36 -0300

    Add Screens files.

---
+++ Uala-Test/Uala-Test/Presentation/Screens/DetailsView/CityDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CityDetailsScreen.swift
+//  Uala-Test
+//
+//  Created by Gastón Montes on 1/9/25.
+//
+
+import SwiftUI
+
+struct CityDetailsScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityDetailsScreen()
+}
---
+++ Uala-Test/Uala-Test/Presentation/Screens/ListView/CityListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CityListScreen.swift
+//  Uala-Test
+//
+//  Created by Gastón Montes on 1/9/25.
+//
+
+import SwiftUI
+
+struct CityListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CityListScreen()
+}
